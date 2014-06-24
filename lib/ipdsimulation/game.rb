7d9625f8ee7c056b967cb5116cb3734b7a63d276
Tudor Pavel <tudorv.pavel@gmail.com>
2014-06-24 20:08:40 +0300

    Update Game class

--- lib/ipdsimulation/game.rb
+++ lib/ipdsimulation/game.rb
@@ -1,8 +1,8 @@
 module IPDSimulation
   class Game
-    def initialize(chromosome1, chromosome2)
-      @player1 = chromosome1
-      @player2 = chromosome2
+    def initialize(individual1, individual2)
+      @player1 = individual1
+      @player2 = individual2
     end
 
     def play_game
@@ -14,18 +14,7 @@ module IPDSimulation
     private
 
       def play_round
-        move1 = rand <= @player1.coop_prob ? '0' : '1'
-        move2 = rand <= @player2.coop_prob ? '0' : '1'
-
-        round_score1, round_score2 = case move1 + move2
-        when '00' then [CC_SCORE, CC_SCORE]
-        when '01' then [CD_SCORE, DC_SCORE]
-        when '10' then [DC_SCORE, CD_SCORE]
-        when '11' then [DD_SCORE, DD_SCORE]
-        end
-
-        @player1.score += round_score1
-        @player2.score += round_score2
+        @player1.play_round_against(@player2)
       end
   end
 end
