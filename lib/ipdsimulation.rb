903f4183c76812b28685f36c9eeabf3ae236ce85
Tudor Pavel <tudorv.pavel@gmail.com>
2014-06-24 22:49:22 +0300

    Comment debugging messages

--- lib/ipdsimulation.rb
+++ lib/ipdsimulation.rb
@@ -7,17 +7,17 @@ require_relative 'ipdsimulation/population'
 module IPDSimulation
   population = Population.new(seed_with: INDIVIDUAL_TYPE)
 
-  max_average = Population.new(seed_with: INDIVIDUAL_TYPE)
-  max_max = Population.new(seed_with: INDIVIDUAL_TYPE)
+  # max_average = Population.new(seed_with: INDIVIDUAL_TYPE)
+  # max_max = Population.new(seed_with: INDIVIDUAL_TYPE)
 
   1.upto(NUM_GENERATIONS).each do |generation|
     population.play_games
 
     puts "Generation #{generation} - Average: #{population.average_fitness.round(2)} - Max: #{population.max_fitness}"
-    puts "Current population: " + population.inspect
+    # puts "Current population: " + population.inspect
 
-    max_average = Population.new(individuals: population.individuals) if population.average_fitness > max_average.average_fitness
-    max_max = Population.new(individuals: population.individuals) if population.max_fitness > max_max.max_fitness
+    # max_average = Population.new(individuals: population.individuals) if population.average_fitness > max_average.average_fitness
+    # max_max = Population.new(individuals: population.individuals) if population.max_fitness > max_max.max_fitness
 
     offspring = Population.new
 
@@ -36,8 +36,8 @@ module IPDSimulation
   end
 
   puts "Final population: " + population.inspect
-  puts "Max Average: #{max_average.average_fitness.round(2)} - Max: #{max_average.max_fitness}"
-  puts max_average.inspect
-  puts "Max Max: #{max_max.average_fitness.round(2)} - Max: #{max_max.max_fitness}"
-  puts max_max.inspect
+  # puts "Max Average: #{max_average.average_fitness.round(2)} - Max: #{max_average.max_fitness}"
+  # puts max_average.inspect
+  # puts "Max Max: #{max_max.average_fitness.round(2)} - Max: #{max_max.max_fitness}"
+  # puts max_max.inspect
 end
--- lib/ipdsimulation/game.rb
+++ lib/ipdsimulation/game.rb
@@ -6,9 +6,9 @@ module IPDSimulation
     end
 
     def play_game
-      puts "** START GAME #{@player1} vs. #{@player2}"
+      # puts "** START GAME #{@player1} vs. #{@player2}"
       NR_ROUNDS.times { play_round }
-      puts "** END GAME #{@player1.score} - #{@player2.score}"
+      # puts "** END GAME #{@player1.score} - #{@player2.score}"
     end
 
     private
