6301b6fb8e6a172cae776d0db978a38411f3f80b
Tudor Pavel <tudorv.pavel@gmail.com>
2014-07-08 10:42:51 +0300

    Reformat the output messages

--- lib/ipdsimulation.rb
+++ lib/ipdsimulation.rb
@@ -52,8 +52,8 @@ module IPDSimulation
   end
 
   # puts "Final population: " + population.inspect
-  puts "Max Average: #{max_average.average_fitness.round(2)} - Max: #{max_average.max_fitness}, Average strategy: #{max_average.average_strategy}"
+  puts "Max Average --> Average fitness: #{max_average.average_fitness.round(2)} - Max: #{max_average.max_fitness}, Average strategy: #{max_average.average_strategy}"
   # puts max_average.inspect
-  puts "Max Max: #{max_max.average_fitness.round(2)} - Max: #{max_max.max_fitness}, Average strategy: #{max_max.average_strategy}"
+  puts "Max Max --> Average fitness: #{max_max.average_fitness.round(2)} - Max: #{max_max.max_fitness}, Average strategy: #{max_max.average_strategy}"
   # puts max_max.inspect
 end
